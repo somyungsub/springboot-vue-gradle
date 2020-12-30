8b53cbcf594282285699dbf35bcc88a276e7e306
somyungsub <gkdldy5@gmail.com>
2020-12-31 01:12:41 +0900

    init : gradle + boot + vue +nuxt

---
+++ backend/app-admin/src/main/java/com/example/appadmin/web/TestController.java
@@ -0,0 +1,41 @@
+package com.example.appadmin.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class TestController {
+
+//  @GetMapping(value = "/")
+//  public String test() {
+//    return "index";
+//  }
+//
+//  @GetMapping(value = "/login")
+//  public String login() {
+//    return "login/index";
+//  }
+
+  @PostMapping(value = "/api/testPost")
+  public ResponseEntity<Map<String, Object>> testPost(@RequestBody Map<String, Object> request) {
+    System.out.println(request);
+    Map<String, Object> res = new HashMap<>();
+    res.put("success", true);
+    return ResponseEntity.ok(res);
+  }
+
+  @PostMapping(value = "/testPost")
+  public ResponseEntity<Map<String, Object>> testPost2(@RequestBody Map<String, Object> request) {
+    System.out.println(request);
+    Map<String, Object> res = new HashMap<>();
+    res.put("success", true);
+    res.put("test2", true);
+    return ResponseEntity.ok(res);
+  }
+}
