efadf28218cda6217a5d13cea5885f8d04b203cc
somyungsub <gkdldy5@gmail.com>
2020-12-30 20:42:25 +0900

    init : gradle + boot + vue +nuxt

---
+++ backend/app-admin/src/test/java/com/example/appadmin/AppAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
